34539e837c11f817d1c2a270408317a76357bb9f
joaquin bidegain <joaquininternacional1@gmail.com>
2023-09-19 15:33:48 -0300

    implementacion boton darkView

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
-import  { NavBar, CenteredText } from './components/index';
+import { NavBar, CenteredText } from './components/index';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React, { useState } from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+
 
 function App() {
+  const [theme, setTheme] = useState('light'); // Estado para el tema
+
+  // Función para cambiar el tema
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  // Crea un tema personalizado basado en el modo actual
+  const customTheme  = createTheme({
+    palette: {
+      mode: theme,
+    },
+  });
+
   return (
-    <div className='root'>
-      <NavBar />
+    <ThemeProvider theme={customTheme }>
+      <CssBaseline />
+      <NavBar toggleTheme={toggleTheme} /> {/* Pasa la función al componente NavBar */}
       <CenteredText />
-    </div>
+    </ThemeProvider>
   );
 }
 
